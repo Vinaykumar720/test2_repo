b7d45bdd0972cd74f59c38dd86386000e2679721
Vinay Kumar <128246992+Vinaykumar720@users.noreply.github.com>
2025-04-05 02:36:40 +0530

    Add files via upload

---
+++ fib.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int first = 1;
+    int second = 2;
+    cout << first << " " << second << " ";
+    for(int i = 0;i<n-2;i++){
+        int next = first + second; 
+        first = second;
+        second = next;
+        cout<<next<<" ";
+    }
+    cout<<"";
+    return 0;
+    
+}
